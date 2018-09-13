4e836708825360c7412631c4386cb4e80dd6878c
Nicolas Coulaud <coulauddev@gmail.com>
2018-09-13 14:09:46 +0200

    rajout securité php

--- index.php
+++ index.php
@@ -10,14 +10,29 @@ if ($_POST) {
 		$errors['name1'] = "Votre nom est requis";
 	}
 
+	if (strlen($_POST['user_name']) < 3)
+	{
+		$errors['name2'] = "Votre nom doit avoir plus de 2 lettres";
+	}
+
 	if (empty($_POST['user_firstname']))
 	{
 		$errors['firstname1'] = "Votre prénom est requis";
 	}
 
+	if (strlen($_POST['user_firstname']) < 3)
+	{
+		$errors['firstname2'] = "Votre prénom doit avoir au moins 2 lettres";
+	}
+
 	if (empty($_POST['user_mail']))
 	{
 		$errors['mail1'] = "Votre mail est requis";
+	}
+
+	if (!filter_var($_POST['user_mail'], FILTER_VALIDATE_EMAIL))
+	{
+		$errors['mail2'] = "Le mail n'a pas le bon format";
 	}
 
 	if (empty($_POST['user_phone']))
@@ -59,16 +74,19 @@ if ($_POST) {
 				<label for="name">Nom :</label>
 				<input required type="text" name="user_name" id="name" placeholder="Nom" value="<?php if(isset($_POST['user_name'])) echo $_POST['user_name']; ?>">
 				<p style="color: red"><?php if (isset($errors['name1']))  echo $errors['name1'];   ?></p>
+				<p style="color: red"><?php if (isset($errors['name2'])) echo $errors['name2'];  ?></p>
 			</div>
 			<div>
 				<label for="firstname">Prénom :</label>
 				<input required type="text" name="user_firstname" id="firstname" placeholder="Prénom" value="<?php if(isset($_POST['user_firstname'])) echo $_POST['user_firstname']; ?>">
 				<p style="color: red"><?php if (isset($errors['firstname1']))  echo $errors['firstname1'];   ?></p>
+				<p style="color: red"><?php if (isset($errors['firstname2'])) echo $errors['firstname2'];  ?></p>
 			</div>
 			<div>
 				<label for="mail">E-mail :</label>
 				<input required type="email" name="user_mail" id="mail" placeholder="E-mail" value="<?php if(isset($_POST['user_mail'])) echo $_POST['user_mail']; ?>">
 				<p style="color: red"><?php if (isset($errors['mail1']))  echo $errors['mail1'];   ?></p>
+				<p style="color: red"><?php if (isset($errors['mail2'])) echo $errors['mail2'];  ?></p>
 			</div>
 			<div>
 				<label for="phone">Téléphone :</label>
